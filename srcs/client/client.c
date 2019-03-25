337cd2bc8952eef50d28f2af06e273fe84f15a82
OleksiiTimofieiev <contrbas@gmail.com>
2019-03-25 09:54:19 +0200

    started working on iterator

--- srcs/client/client.c
+++ srcs/client/client.c
@@ -53,8 +53,12 @@ bool	validation_of_program_arguments(int argc, char **argv)
 void	client_configuration(char **argv, char *id, int *delay, short *port, unsigned char **iterator)
 {
 	memcpy(id, argv[1], 8);
-	/* usleep() takes microseconds, 
-	so we have to multiply the input by 1000 in order to sleep in milliseconds. */
+
+	/* 
+	** usleep() takes microseconds, 
+	** so we have to multiply the input by 1000 in order to sleep in milliseconds. 
+	*/
+
 	*delay		 = atoi(argv[2]) * 1000;
 	*port 		 = atoi(&argv[3][3]);
 	*iterator	 = (unsigned char *)malloc(sizeof(unsigned char) * INITIAL_ITERATOR_SIZE);
