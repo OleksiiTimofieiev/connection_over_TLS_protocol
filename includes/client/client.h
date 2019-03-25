dc975d1dc868ae816f2285851bc06050727fd943
contrbas@gmail.com <K4rmU78B>
2019-03-25 11:38:38 +0200

    client.h

---
+++ includes/client/client.h
@@ -0,0 +1,14 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+
+#endif
--- srcs/client/client.c
+++ srcs/client/client.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h> 
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <stdbool.h>
+#include "../../includes/client/client.h"
 
 // TODO: delete all comments;
 // TODO: defines to header;
