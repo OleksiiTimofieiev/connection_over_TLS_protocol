7240b5c99bc8d4244ed1c7efd508e2496c2bc532
contrbas@gmail.com <K4rmU78B>
2019-03-27 15:16:15 +0200

    for linux

--- srcs/server/aes_decryption.c
+++ srcs/server/aes_decryption.c
@@ -2,12 +2,11 @@
 
 void 	aes_decrypt(unsigned char *iv, unsigned char *key, unsigned char *input, unsigned char *output)
 {
-	int ret;
 	mbedtls_aes_context aes;
 
 	mbedtls_aes_init(&aes);
 
-	ret = mbedtls_aes_setkey_dec(&aes, key, 256);
+	mbedtls_aes_setkey_dec(&aes, key, 256);
 
-	ret = mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, INITIAL_PACKET_SIZE + DIGEST_SIZE, iv, input, output);
+	mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, INITIAL_PACKET_SIZE + DIGEST_SIZE, iv, input, output);
 }
