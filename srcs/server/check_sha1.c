4ef28f2404d2d928705c709ede997bdd1c6e5791
contrbas@gmail.com <K4rmU78B>
2019-04-03 15:47:51 +0300

    wtf with ubuntu

--- srcs/server/check_sha1.c
+++ srcs/server/check_sha1.c
@@ -2,11 +2,12 @@
 
 bool	check_sha1_sum(unsigned char *checksum, const unsigned char * const decrypted_full_packet)
 {
+	int j = 256;
 	int i = 0;
 
 	while (i < 16)
 	{
-		if (checksum[i] != decrypted_full_packet[i])
+		if (checksum[i] != decrypted_full_packet[j + i])
 			break;
 		i++;
 
