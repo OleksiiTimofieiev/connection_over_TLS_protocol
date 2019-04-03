47d1be5f329b8f1fc3e12bd2f98f6ba51df17a2f
contrbas@gmail.com <K4rmU78B>
2019-04-03 14:12:59 +0300

    correction of the .rodata

--- srcs/server/check_sha1.c
+++ srcs/server/check_sha1.c
@@ -2,15 +2,17 @@
 
 bool	check_sha1_sum(unsigned char *checksum, unsigned char *decrypted_full_packet)
 {
-	int i = 0;
+	int i = 256;
+	int j = 0;
 
-	while (i < 16)
+	while (i < 256)
 	{
 		if (checksum[i] != decrypted_full_packet[i])
 			break;
 		i++;
+		j++;
 	}
-	if (i == 16)
+	if (j == 16)
 		return (true);
 	return (false);
 }
